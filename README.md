dff1b147d3371dfcf5a774698ccbcb64632f746a
Kesav Nallan <kesav.nallan@gmail.com>
2017-12-18 15:35:55 -0500

    Basic Consumer

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # RabbitMQ
-Download Rabbit MQ (Binary Version i.e dump)
-Install ERLang
-Set the ERLANG_HOME
-goto rabbit sbin directory and type rabbitmq-plugins enable rabbitmq_management
-default user: guest
-default password: guest
+	Download Rabbit MQ (Binary Version i.e dump)
+	Install ERLang
+	Set the ERLANG_HOME
+	goto rabbit sbin directory and type rabbitmq-plugins enable rabbitmq_management
+	default user: guest
+	default password: guest
--- src/main/java/com/kesav/RabbitMQ/ConsumerEx1.java
+++ src/main/java/com/kesav/RabbitMQ/ConsumerEx1.java
@@ -1,19 +1,38 @@
 package com.kesav.RabbitMQ;
 
-import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.QueueingConsumer.Delivery;
 
 public class ConsumerEx1 {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception{
 		ConnectionFactory factory = new ConnectionFactory();
 		factory.setHost("localhost");
 		factory.setUsername("guest");
 		factory.setPassword("guest");
 		Connection connection = factory.newConnection();
 		Channel channel = connection.createChannel();
+		QueueingConsumer consumer = new QueueingConsumer(channel);
+		channel.basicConsume("KesavTestQueue", consumer);
 		
+		boolean removeAllUpTo = true;
+		while(true) {
+			Delivery delivery = consumer.nextDelivery();
+			if(delivery == null) break;
+			if(processMessage(delivery)){
+				long deliveryTag = delivery.getEnvelope().getDeliveryTag();
+				channel.basicAck(deliveryTag, removeAllUpTo);
+			}
+		}		
+	}
+
+	private static boolean processMessage(Delivery delivery) throws UnsupportedEncodingException {
+		String msg = new String(delivery.getBody(), "UTF-8");
+		System.out.println("[x] Recv: redeliver = " +delivery.getEnvelope().isRedeliver() + ". msg = " + msg);
+		return true;
 	}
 }
